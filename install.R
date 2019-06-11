a7741501211edd5e0658846bbd4a3b17b8899e7b
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-06-11 10:11:48 +0100

    update

--- install.R
+++ install.R
@@ -1,3 +1,9 @@
+
 install.packages("ggplot2")
 install.packages("readr")
+install.packages("dplyr")
 install.packages("magrittr")
+install.packages("ggthemes")
+install.packages("stingr")
+install.packages("tools")
+install.packages("ggrepel")
--- script_vis_4.R
+++ script_vis_4.R
@@ -1,11 +1,11 @@
 library(ggplot2)
-library(ggthemes)
-library(ggrepel)
 library(readr)
 library(dplyr)
+library(magrittr)
+library(ggthemes)
 library(stringr)
 library(tools)
-library(magrittr)
+library(ggrepel)
 
 full_trains <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-26/full_trains.csv")
 
